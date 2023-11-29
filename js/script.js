631958ba37190e32094f3d50744fabde43d49182
AleDataAnalyst <criptolmin@gmail.com>
2023-11-29 11:46:55 +0100

    npm install jsdom

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+const { JSDOM } = require('jsdom');
+
 const loaderEl = document.getElementById("#js-preloader");
 const gameList = document.querySelector("#juegos");
 const btnCargarMasJuegos = document.querySelector(".btn-cargarmas");
 // url a games en doc por fechas
-const urlGames = 'https://api.rawg.io/api/games?key=${apiKey}&dates=2023-01-01,2023-11-29&ordering=-added';
+const urlGames = `https://api.rawg.io/api/games?key=${APIKEY}&dates=2023-01-01,2023-11-29&ordering=-added`;
 
 console.log(urlGames, 'check');
