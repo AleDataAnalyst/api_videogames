7ddd245493c4f284a3497f755dfbef26dcf5fe1b
AleDataAnalyst <criptolmin@gmail.com>
2023-11-29 11:27:31 +0100

    avances javascript

---
+++ js/prod.env.js
@@ -0,0 +1,6 @@
+// API Key de RAWG.io
+// https://api.rawg.io/docs
+// https://rawg.io/apidocs
+// https://rawg.io/@criptol/apikey
+const apiKey = 'eefbd8dac0b744f5b0cc9f499d593e49';
+
--- js/script.js
+++ js/script.js
@@ -1,38 +1,7 @@
-const apiKey = 'eefbd8dac0b744f5b0cc9f499d593e49';
+const loaderEl = document.getElementById("#js-preloader");
+const gameList = document.querySelector("#juegos");
+const btnCargarMasJuegos = document.querySelector(".btn-cargarmas");
+// url a games en doc por fechas
+const urlGames = 'https://api.rawg.io/api/games?key=${apiKey}&dates=2023-01-01,2023-11-29&ordering=-added';
 
-// Función para obtener información de plataformas
-function obtenerPlataformas() {
-    const url = `https://api.rawg.io/api/platforms?key=${apiKey}`;
-
-    $.get(url, function (data) {
-        mostrarDatos('plataformas', data);
-    });
-}
-
-// Función para obtener información de juegos
-function obtenerJuegos() {
-    const fechas = '2019-09-01,2019-09-30';
-    const plataformas = [18, 1, 7];
-    const url = `https://api.rawg.io/api/games?key=${apiKey}&dates=${fechas}&platforms=${plataformas.join(',')}`;
-
-    $.get(url, function (data) {
-        mostrarDatos('juegos', data);
-    });
-}
-
-// Función para mostrar datos en el HTML
-function mostrarDatos(id, data) {
-    const container = document.getElementById(id);
-
-    if (data && data.results) {
-        container.innerHTML = '<pre>' + JSON.stringify(data.results, null, 2) + '</pre>';
-    } else {
-        container.innerHTML = '<p>Error al obtener datos.</p>';
-    }
-}
-
-// Llamadas a las funciones para obtener datos al cargar la página
-$(document).ready(function () {
-    obtenerPlataformas();
-    obtenerJuegos();
-});
+console.log(urlGames, 'check');
