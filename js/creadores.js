a2d6415f53b60a267bcee6fb275f9a3a322b7d78
AleDataAnalyst <criptolmin@gmail.com>
2023-12-19 00:17:33 +0100

    Actualizaciones de código nuevas

---
+++ js/creadores.js
@@ -0,0 +1,67 @@
+const apiKey = "eefbd8dac0b744f5b0cc9f499d593e49";
+const baseUrl = "https://api.rawg.io/api/games";
+
+// Función buscar creadores
+async function fetchAndPrintCreators(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayCreators(data.results);
+        } else {
+            console.log('Error buscando creadores:', response.status);
+        }
+    } catch (error) {
+        console.error('Error buscando creadores:', error);
+    }
+}
+
+// Función para mostrar creadores
+function displayCreators(creators) {
+    let creatorsHtml = '';
+    creators.forEach(creator => {
+        creatorsHtml += `
+            <div class="creator">
+                <h3>${creator.name}</h3>
+                <img src="${creator.image}" alt="${creator.name}">
+                <p>Games Count: ${creator.games_count}</p>
+            </div>`;
+    });
+    document.getElementById('creatorsList').innerHTML = creatorsHtml;
+}
+
+// Llamada inicial para cargar la primera página de creadores
+const initialUrl = `${baseUrl}?key=${apiKey}&page=1`;
+fetchAndPrintCreators(initialUrl);
+
+// Paginación
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+
+btnSiguiente.addEventListener('click', () => {
+    if (pagina < 1000) {
+        pagina += 1;
+        const paginationUrl = `${baseUrl}?key=${apiKey}&dates=2020-01-01,2023-12-08&ordering=-added&page=${pagina}`;
+        fetchAndPrintGames(paginationUrl);
+    }
+});
+
+btnAnterior.addEventListener('click', () => {
+    if (pagina > 1) {
+        pagina -= 1;
+        const paginationUrl = `${baseUrl}?key=${apiKey}&dates=2020-01-01,2023-12-08&ordering=-added&page=${pagina}`;
+        fetchAndPrintGames(paginationUrl);
+    }
+});
+
+// Función para añadir contenido de modal al body
+function appendModalContentToBody(modalContent) {
+    document.body.insertAdjacentHTML('beforeend', modalContent);
+}
+
+// Función para inicializar y mostrar modal
+function initializeAndShowModal(creatorId) {
+    const modal = new bootstrap.Modal(document.getElementById(`exampleModal-${creatorId}`));
+    modal.show();
+}
--- js/juegos.js
+++ js/juegos.js
@@ -1,4 +1,60 @@
 const apiKey = "eefbd8dac0b744f5b0cc9f499d593e49";
+const baseUrl = "https://api.rawg.io/api/games";
+
+// Función para buscar juegos basados en search query y número de página
+async function fetchAndPrintGames(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayGames(data.results);
+        } else {
+            console.log('Error buscando juegos:', response.status);
+        }
+    } catch (error) {
+        console.error('Error buscando juegos:', error);
+    }
+}
+
+// Función para mostrar juegos en una página
+function displayGames(games) {
+    let gamesHtml = '';
+    games.forEach(game => {
+        gamesHtml += `
+            <div class="card mb-4 p-3" data-id="${game.id}">
+                <img src="${game.background_image}" class="card-img" alt="${game.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${game.name}</h5>
+                    <p class="card-text">Género: <span>${getGenres(game.genres)}</span></p>
+                    <p class="card-text">Calificación: <li><i class="fa fa-star"></i></li><span class="rating">${getRatings(game.ratings)}</span></li>
+                    <p class="card-text">Lanzamiento: <span>${game.released}</span></p>
+                </div>
+            </div>`;
+    });
+    document.getElementById('gamesCards').innerHTML = gamesHtml;
+
+    // Añadir event listener para clicks en tarjetas para abrir modal
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('click', () => {
+            const gameId = card.dataset.id;
+            openModal(gameId);
+        });
+    });
+}
+
+// Event listener para botón de búsqueda
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click', () => {
+    const searchTerm = document.getElementById('searchInput').value.trim();
+    if (searchTerm) {
+        const searchUrl = `${baseUrl}?key=${apiKey}&search=${encodeURIComponent(searchTerm)}`;
+        fetchAndPrintGames(searchUrl);
+    }
+});
+
+// Llamada inicial para cargar la primera página de juegos
+const initialUrl = `${baseUrl}?key=${apiKey}&dates=2020-01-01,2023-12-08&ordering=-added&page=1`;
+fetchAndPrintGames(initialUrl);
 
 // Paginación
 let pagina = 1;
@@ -8,154 +64,95 @@ const btnSiguiente = document.getElementById('btnSiguiente');
 btnSiguiente.addEventListener('click', () => {
     if (pagina < 1000) {
         pagina += 1;
-        cargarJuegos();
+        const paginationUrl = `${baseUrl}?key=${apiKey}&dates=2020-01-01,2023-12-08&ordering=-added&page=${pagina}`;
+        fetchAndPrintGames(paginationUrl);
     }
 });
 
 btnAnterior.addEventListener('click', () => {
     if (pagina > 1) {
         pagina -= 1;
-        cargarJuegos();
+        const paginationUrl = `${baseUrl}?key=${apiKey}&dates=2020-01-01,2023-12-08&ordering=-added&page=${pagina}`;
+        fetchAndPrintGames(paginationUrl);
     }
 });
 
 // Géneros
 function getGenres(genres) {
-    // Revisar si es array
     if (Array.isArray(genres)) {
-        // Extraer nombres de género y unirlos en un string
         return `${genres.map(genre => genre.name).join(', ')}`;
     } else {
-        // Retornar valor por defecto si genres no es array
         return 'N/A';
     }
 }
 
 // Ratings
 function getRatings(ratings) {
-    // Revisar si es array
     if (Array.isArray(ratings) && ratings.length > 0) {
-        // Extraer valores de rating y unirlos en string
         return `${ratings.map(rating => rating.title).join(', ')}`;
     } else {
-        // Retornar valor por defecto si ratings no es array o está vacío
         return 'N/A';
     }
 }
 
-// Función Cargar Juegos
-const cargarJuegos = async () => {
+// Función para buscar detalles de juegos
+async function fetchGameDetails(gameId) {
+    const urlGameDetails = `${baseUrl}/${gameId}?key=${apiKey}`;
     try {
-        const respuesta = await fetch(`https://api.rawg.io/api/games?key=${apiKey}&dates=2020-01-01,2023-12-08&ordering=-added&page=${pagina}`);
-
-        if (respuesta.ok) {
-            const datos = await respuesta.json();
-
-            let juegos = '';
-            datos.results.forEach(juegoData => {
-                juegos += `
-                    <div class="card mb-4 p-3" data-id="${juegoData.id}">
-                        <img src="${juegoData.background_image}" class="card-img" alt="${juegoData.name}">
-                        <div class="card-body">
-                            <h5 class="card-title">${juegoData.name}</h5>
-                            <p class="card-text">Género: <span>${getGenres(juegoData.genres)}</span></p>
-                            <p class="card-text">Calificación: <span>${getRatings(juegoData.ratings)}</span></p>
-                            <p class="card-text">Lanzamiento: <span>${juegoData.released}</span></p>
-                        </div>
-                    </div>`;
-            });
-
-            // Agregar el contenido al contenedor en tu HTML
-            document.getElementById('gamesCards').innerHTML = juegos;
-        } else if (respuesta.status === 401) {
-            console.log('API Key incorrecta');
-        } else if (respuesta.status === 404) {
-            console.log('El videojuego que buscas no existe');
-        } else {
-            console.log('Hubo un error desconocido');
-        }
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-
-// Llamada inicial para cargar la primera página
-cargarJuegos();
-
-// Modal descripción individual
-// Añadir event listener para clicks de tarjeta
-document.getElementById('gamesCards').addEventListener('click', (event) => {
-    const card = event.target.closest('.card');
-    if (card) {
-        const gameId = card.dataset.id;
-        if (gameId) {
-            event.preventDefault();
-            openModal(gameId);
-        }
-    }
-});
-
-// Función para fetch y mostrar información detallada en un modal
-async function openModal(gameId) {
-    try {
-        const urlGameDetails = `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`;
-    
-        // Fetch game details
         const responseDetails = await fetch(urlGameDetails);
         if (!responseDetails.ok) {
             throw new Error(`Error HTTP! Estado: ${responseDetails.status}`);
         }
-        const gameDetails = await responseDetails.json();
+        return await responseDetails.json();
+    } catch (error) {
+        console.error('Error buscando detalles de juegos:', error);
+    }
+}
 
-        // Mostrar game details en modal
-        const modalContent = `
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header px-4">
-                            <h3 class="modal-title fs-5" id="exampleModalLabel">${gameDetails.name}</h3>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body px-4">
-                            <p>${gameDetails.description}</p>
-                        </div>
-                        <div class="modal-footer px-4">
-                            <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Cerrar</button>
-                            <button type="button" id="btn-web" class="btn btn-primary rounded-pill">
-                            <a href="${gameDetails.website}" class="btn text-center" target="_blank">Visitar web</a></button>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-
-        // Añadir el contenido modal content al document body
-        document.body.insertAdjacentHTML('beforeend', modalContent);
-
-        // Inicializar el modal usando métodos Bootstrap modal
-        const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
-        modal.show();
+// Función para crear y mostrar modal
+async function openModal(gameId) {
+    try {
+        const gameDetails = await fetchGameDetails(gameId);
+        const modalContent = createModalContent(gameDetails);
+        appendModalContentToBody(modalContent);
+        initializeAndShowModal(gameId);
     } catch (error) {
         console.error('Error al buscar los detalles del juego:', error);
     }
 }
 
-// Buscador
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
+// Función para crear contenido de modal
+function createModalContent(gameDetails) {
+    return `
+        <div class="modal fade" id="exampleModal-${gameDetails.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header px-4">
+                        <h3 class="modal-title fs-5" id="exampleModalLabel">${gameDetails.name}</h3>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body px-4">
+                        <p>${gameDetails.description}</p>
+                    </div>
+                    <div class="modal-footer px-4">
+                        <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button" id="btn-web" class="btn btn-primary rounded-pill">
+                            <a href="${gameDetails.website}" class="btn text-center" target="_blank">Visitar web</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
 
-searchButton.addEventListener('click', () => {
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm) {
-        // Borrar resultados anteriores
-        document.getElementById('gamesCards').innerHTML = '';
+// Función para añadir contenido de modal al body
+function appendModalContentToBody(modalContent) {
+    document.body.insertAdjacentHTML('beforeend', modalContent);
+}
 
-        // Usar el parámetro search en la API URL
-        const urlWithSearch = `${urlGames}&search=${encodeURIComponent(searchTerm)}`;
-
-        // Fetch y mostrado de juegos con la URL actualizada
-        fetchAndPrintGames(urlWithSearch);
-    }
-});
+// Función para inicializar y mostrar modal
+function initializeAndShowModal(gameId) {
+    const modal = new bootstrap.Modal(document.getElementById(`exampleModal-${gameId}`));
+    modal.show();
+}
